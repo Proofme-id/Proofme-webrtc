6c1ab8baeccee99e2316af8cd8cba50b365572f7
Daniel Leushuis <danieleushuis@gmail.com>
2023-03-23 09:51:47 +0100

    editted requested credentials object

--- src/interfaces/requestedCredentials.interface.ts
+++ src/interfaces/requestedCredentials.interface.ts
@@ -5,17 +5,12 @@ import { IRequestedCredentialKey } from "./requestedCredentialKey.interface";
 export interface IRequestedCredentials {
     credentials: IRequestedCredentialKey[];
     description?: string;
-    pgpDecrypt?: boolean;
     purpose?: EProofmeDataPurpose;
     proof?: {
         holder: string;
         nonce: number;
         signature?: string;
         type: string;
-    }
-    minimumRequired?: {
-        data: string[];
-        amount: number;
     }
     requester?: string;
     storage?: EProofmeDataStorage
